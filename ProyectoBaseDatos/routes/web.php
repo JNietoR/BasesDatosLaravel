7c17506d52e07b52e1e38af7f7acb2b03501bfa7
JNietoR <jorge.nieto35@gmail.com>
2023-10-22 12:15:13 +0200

    listado de rutas y sus metodos en el controlador

--- ProyectoBaseDatos/routes/web.php
+++ ProyectoBaseDatos/routes/web.php
@@ -2,17 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\PageController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'home'])->name('home');
+
+Route::get('categoria/{category}', [PageController::class, 'category'])->name('page.category');
+Route::get('etiqueta/{tag}',       [PageController::class, 'tag'])->name('page.tag');
+Route::get('hilo/{thread}',        [PageController::class, 'thread'])->name('page.thread');
+
