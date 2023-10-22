06590ac3ca34065cbc663f4d629ad368d9caa49d
JNietoR <jorge.nieto35@gmail.com>
2023-10-22 15:44:16 +0200

    optimización de consulstas haciendolas en el controlador en lugar de la vista

--- ProyectoBaseDatos/app/Http/Controllers/PageController.php
+++ ProyectoBaseDatos/app/Http/Controllers/PageController.php
@@ -12,23 +12,44 @@ class PageController extends Controller
     
     public function home(){
 
-        $threads = Thread::orderBy('id', 'DESC')->paginate();
+        $threads = Thread::orderBy('id', 'DESC')
+            ->with('category' ,'tags', 'user')
+            ->withCount('comments')
+            ->paginate();
 
         return view('home', compact('threads'));
     }
 
     public function category(Category $category){
-        $threads = $category->threads()->orderBy('id', 'DESC')->paginate();
+
+        $threads = $category
+            ->threads()
+            ->orderBy('id', 'DESC')
+            ->with('category' ,'tags', 'user')
+            ->withCount('comments')
+            ->paginate();
         return view('category', compact('category', 'threads'));
     }
 
     public function tag(Tag $tag){
-        $threads = $tag->threads()->orderBy('id', 'DESC')->paginate();
+
+        $threads = $tag
+        ->threads()
+        ->orderBy('id', 'DESC')
+        ->with('category' ,'tags', 'user')
+        ->withCount('comments')
+        ->paginate();
         return view('tag', compact('tag', 'threads'));
     }
 
     public function thread(Thread $thread){
-        return view('thread', compact('thread'));
+
+        $comments = $thread
+        ->comments()
+        ->orderBy('id', 'DESC')
+        ->with('user')
+        ->get();
+        return view('thread', compact('thread', 'comments'));
     }
 
 
