eab90929dee057884ced79206a99cb731ff8c2ae
JNietoR <jorge.nieto35@gmail.com>
2023-10-22 11:01:32 +0200

    modificación de la estructura de tablas y migración desde laravel a la base de datos

--- ProyectoBaseDatos/database/migrations/2023_10_22_084835_create_threads_table.php
+++ ProyectoBaseDatos/database/migrations/2023_10_22_084835_create_threads_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('threads', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->text('body');
+
             $table->timestamps();
         });
     }
