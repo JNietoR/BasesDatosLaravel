004d8806bd6586f6d98ab45d290fade20f638f55
JNietoR <jorge.nieto35@gmail.com>
2023-10-22 12:01:27 +0200

    organización de datos modificación del seeder y modelos para que factory no alimente la base con una gran cantidad de información

--- ProyectoBaseDatos/app/Models/Thread.php
+++ ProyectoBaseDatos/app/Models/Thread.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Thread extends Model
 {
     use HasFactory;
+
+    // relación 1:M
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
+    //relación M:M
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
 }
